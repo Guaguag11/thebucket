54e2c121d28914cc8c207df4aba62857b444f825
Guaguag11 <dianagarzaguz11@gmail.com>
2024-04-16 17:44:27 -0600

    added footer and cards

--- index.js
+++ index.js
@@ -8,11 +8,3 @@ button.addEventListener('click', function(){
     console.log('si se pico');
     title.classList.toggle('header__title-visible');
 });
-
-let titleText = document.querySelector('.header__title').textContent
-
-if (titleText = "Bienvenides a todes"){
-    console.log("si jala");
-} else {
-    console.log("no jala");
-}
